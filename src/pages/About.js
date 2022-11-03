9d61c75da7dfb2aca8d11c68f326a0a7d966a2fc
Sabri Alabouch <sabri544@msn.com>
2022-11-03 08:55:47 +0100

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>APropos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt quia
+        provident reprehenderit incidunt rem molestias temporibus optio illum
+        nobis officiis maxime possimus, quam repellendus aliquam, ratione
+        mollitia adipisci accusantium reiciendis ipsum omnis! Ab odit harum
+        neque non ipsum facere ex esse necessitatibus placeat, ducimus est
+        eligendi officia molestiae ea, quaerat magni adipisci nulla illum qui
+        quo consectetur unde voluptatum! Adipisci accusantium nostrum at a quod
+        sed repellat sapiente. Itaque distinctio, molestiae dolores quos facere
+        perspiciatis nulla temporibus! Quas eius nulla id ea fugiat optio illum
+        ipsum! Magni ullam sunt, quibusdam nam quisquam nulla eius, eligendi
+        reprehenderit adipisci blanditiis, perspiciatis eaque.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+        soluta deserunt ipsa et mollitia architecto quod eveniet natus porro!
+        Vitae molestiae sapiente quibusdam est doloribus eveniet recusandae
+        excepturi distinctio reprehenderit nam non, quos, tempore nisi iusto
+        repellat exercitationem libero laudantium, praesentium fugit nulla!
+        Impedit, nulla sit id sed maxime amet?
+      </p>
+    </div>
+  );
+};
+
+export default About;
