f8ce4cc21bde021cd98253bc17f40e7f8d905d6b
Sabri Alabouch <sabri544@msn.com>
2022-11-05 10:21:48 +0100

    fetch et map

---
+++ src/components/Countries.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Countries = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((res) => setData(res.data));
+  });
+
+  return (
+    <div className="countries">
+      <h1>Countries</h1>
+      <ul>
+        {data.map((country, index) => (
+          <li key={index}>{country.translations.fra.common}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Countries;
